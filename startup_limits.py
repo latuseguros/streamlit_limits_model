176cdcdb307ab4f7ea93d165e9586fce5c06d525
HDNovak <51675829+HDNovak@users.noreply.github.com>
2022-11-28 18:58:49 -0300

    Update startup_limits.py

--- startup_limits.py
+++ startup_limits.py
@@ -59,6 +59,3 @@ if st.button('Calculate Limits, Risk and Premium'):
         premium = float(limits*(RoL/100))
     
     st.write(f'Limits: '+'$'+format(limits,',.2f'))
-    st.write(f'Risk: {risk}')
-    st.write(f'RoL: '+"{0:.0%}".format(RoL/100))
-    st.write(f'Premium: '+'$'+format(premium,',.2f'))
